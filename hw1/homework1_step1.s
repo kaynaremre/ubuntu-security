64b35d30c80055482cee5ad10e9bed391806e484
kaynaremre <emrekaynar_8@hotmail.com>
2020-11-12 22:02:57 +0000

    osas

---
+++ hw1/homework1_step1.s
@@ -0,0 +1,70 @@
+	.file	"homework1_step1.c"
+	.section	.rodata
+.LC0:
+	.string	"now inside secret()!"
+	.text
+	.globl	secret
+	.type	secret, @function
+secret:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	secret, .-secret
+	.section	.rodata
+.LC1:
+	.string	"now inside osas entrance()!"
+	.text
+	.globl	entrance
+	.type	entrance, @function
+entrance:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$.LC1, %edi
+	call	puts
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	entrance, .-entrance
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movl	$0, %eax
+	call	entrance
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.9.4"
+	.section	.note.GNU-stack,"",@progbits
