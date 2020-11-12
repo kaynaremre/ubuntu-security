8342c580fa8dcb2dd8a00bd6b1adfc228c7807b4
kaynar <emrekaynar_8@hotmail.com>
2020-11-13 01:06:35 +0300

    osass

---
+++ hw1/wikiadam.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+char           A[8] = "";
+unsigned short B    = 1979;
+
+strcpy(A, "arabellekinisikeyimoc");
+
+printf("%d", B);
+
+return 0;
+}
